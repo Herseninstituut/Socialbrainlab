b4b65f8b851ca26e1d05362540f4204ca8d561ba
Rajeev Rajendran <r.rajendran@nin.knaw.nl>
2019-07-29 18:09:39 +0200

    made functions more readable

--- Rajeev/image_analysis/calcium/two_photon/response/responsive_cells.m
+++ Rajeev/image_analysis/calcium/two_photon/response/responsive_cells.m
@@ -1,115 +1,79 @@
 %% created by Rajeev Rajendran
-% 2019_06_27 
-% Open directory
-% split folders
-function responsive_cells(stim_first,stim_last,pre_stim_dur,stim_dur,post_stim_dur) % 
+% 2019_06_27
+% Finds responsive cells based on the given stimuli, calculated by Wilcoxon
+% sign rank test between paired trials of averaged sugnals between pre-
+% onset and post- stim time windows. Correct stim indices and appropriate
+% stim duration details are needed as input arguments.
 
+
+function responsive_cells(stim_first,stim_last,pre_stim_dur,stim_dur,...
+    post_stim_dur)
+format compact
+% select the correct directory and make it current
 [fn , pn] = uigetfile('*_SPSIG.mat');
 cd(pn)
 
-%% Load data files
+% load data files
 SPSIG_file = load(uigetfile('*_SPSIG.mat')); % signal file
 info = load(uigetfile('*_normcorr.mat', 'info')); % normcorr mat file
 quad = load(uigetfile('*quad_split*.mat')); % run file
 stim=floor((info.info.frame)/3); % get stim onset and offset frames
 
-%% calculate speed
+% calculate speed
 run=['quad.quad_',pn(end-6:end-1)];
 run=eval(run);
 ra = 10;
 dist = (2*pi*ra)*run/1000; %in cms
 speed = (dist)*SPSIG_file.freq; % in cms/s
 
-%% manually check by opening stim
-% prompt='Which is the first stim onset index?';
-% stim_first=input(prompt); % first stim onset
-% prompt='Which is the last stim onset index?';
-% stim_last=input(prompt); % last stim onset
 n=size(SPSIG_file.sig,1); % total frames available
 
-% Selecting the stim on and off frames
+% select stim on and off frames
 Ts=size((stim_first:2:stim_last),2);
 for i=1:Ts
     StimOn(i,1)=stim((stim_first+(2*i-2)),1);
     StimOff(i,1)=stim((stim_first+(2*i-2))+1,1);
 end
 
-% label frames when stim is on
-m=1;
-for i=1:n
-    if i >= StimOn(m,1) && i < StimOff(m,1)
-        st_fr(i,1)=1;
-    elseif i == StimOff(m,1)
-        st_fr(i,1)=1;
-        if m < Ts
-            m=m+1;
-        end
-    else
-        st_fr(i,1)=0;
-    end
-end
-%% Extract stim frames
-% Prestim = 4s, stim 2s frames, post stim 4s
-% prompt='how many seconds before stim onset?';
-% pre_stim_dur=input(prompt); % in seconds
-% prompt='how many seconds of stim?';
-% stim_dur=input(prompt); % in seconds
-% prompt='how many seconds after stim offset?';
-% post_stim_dur=input(prompt); % in seconds
-%% distribute the signals into pre and stim groups for all stims
-Tf_pre=ceil((pre_stim_dur)*SPSIG_file.freq); % no. of frames before stim onset
-Tf_st=ceil((stim_dur+post_stim_dur)*SPSIG_file.freq); % no. of frames from stim onset
+% distribute the signals into pre and stim groups for all stims
+% no. of frames before stim onset
+Tf_pre=ceil((pre_stim_dur)*SPSIG_file.freq);
+% no. of frames from stim onset
+Tf_st=ceil((stim_dur+post_stim_dur)*SPSIG_file.freq);
+% no. of ROIs
 Tr=size(SPSIG_file.sig,2);
+% make blank array for stats
 stat_arr=nan(Ts,2,Tr);
+% enter correct values (:,1,:) for baseline, (:,2,:) for post stim onset
 for i=1:Tr % for each ROI
     for j=1:Ts % for each stim
         if (StimOn(j)-Tf_pre)>=1
-            stat_arr(j,1,i)=mean(SPSIG_file.sig((StimOn(j)-Tf_pre):(StimOn(j)-1),i));
+            stat_arr(j,1,i)=mean(SPSIG_file.sig((StimOn(j)-Tf_pre):...
+                (StimOn(j)-1),i));
         end
-            stat_arr(j,2,i)=mean(SPSIG_file.sig(StimOn(j)+2:(StimOn(j)+Tf_st),i));
-            % +2 for ~ half a second shift into the stim
+        stat_arr(j,2,i)=mean(SPSIG_file.sig(StimOn(j)+2:(StimOn(j)...
+            +Tf_st),i));
+        % 2 added for ~ half a second shift into the stim
     end
 end
 
-%% shapiro wilk
-% sw.stat={};
-% for i=1:Tr % for each ROI
-%     [H, pValue, SWstatistic]=swtest(stat_arr(:,1,i),0.05);
-%     sw.stat.H(i,1)=H;
-%     sw.stat.p(i,1)=pValue;
-%     sw.stat.SW(i,1)=SWstatistic;
-%     [H, pValue, SWstatistic]=swtest(stat_arr(:,2,i),0.05);
-%     sw.stat.H(i,2)=H;
-%     sw.stat.p(i,2)=pValue;
-%     sw.stat.SW(i,2)=SWstatistic;
-% end
-%% Wilcoxon rank sum test (for non-paired non uniform samples) 
-% % equivalent to the Mann-Whitney U test
-% 
-% rs.val={};
-% resp=zeros(1,Tr);
-% resp_rois=[];
-% c=1;%count
-% for i=1:Tr % for each ROI
-%     [p,h,stats]=ranksum(stat_arr(:,1,i),stat_arr(:,2,i),'alpha',0.05,'tail','left');
-%     rs.val.p(i)=p;
-%     if rs.val.p(i)<0.05
-%         resp(1,i)=1;
-%         resp_rois(c)=i;
-%         c=c+1;
-%     end
-%     rs.val.h(i)=h;
-%     rs.val.stat(i)=stats;
-% end
-% tot_roi=sum(rs.val.h);
 
-%% Wilcoxon sign rank test (for paired non uniform samples) 
+% Wilcoxon sign rank test (for paired non uniform samples) since all roi
+% signal distribution of either baseline or post stim was not normal
+% make blank signrank stat cell variable
 sr.val={};
+% binary inputs for all rois, 1= responsive, else 0
 resp=zeros(1,Tr);
+% index of responsive rois
 resp_rois=[];
+% incrementing count for responsive rois
 c=1;
-for i=1:Tr % for each ROI
-    [p,h,stats] = signrank(stat_arr(:,1,i),stat_arr(:,2,i),'alpha',0.05,'tail','left');
+for i=1:Tr % for each roi
+    % tail is left since signrank(x,y) y>x, we expect the signal to
+    % increase after the stim onset. p = p-value, h = hypothesis (1
+    % indicates null hypothesis is rejected else 0).
+    [p,h,stats] = signrank(stat_arr(:,1,i),stat_arr(:,2,i),'alpha',0.05,...
+        'tail','left');
     sr.val.p(i)=p;
     if sr.val.p(i)<0.05
         resp(1,i)=1;
@@ -120,8 +84,12 @@ for i=1:Tr % for each ROI
     sr.val.stat(i)=stats;
 end
 
-%%
-save(fn, 'resp','resp_rois','sr','StimOn','stat_arr','speed', 'Tf_pre', 'Tf_st', '-append')
+%
+disp(['No. of responsive rois: ', num2str(size(resp_rois,2))]);
+disp(['Percent of responsive rois: ',num2str(round(100*size(resp_rois,2)...
+    /Tr),2)]);
+save(fn, 'resp','resp_rois','sr','StimOn','stat_arr','speed', 'Tf_pre',...
+    'Tf_st', '-append')
 clear
-clc
+
 
--- Rajeev/image_analysis/calcium/two_photon/running/quad_split3.m
+++ Rajeev/image_analysis/calcium/two_photon/running/quad_split3.m
@@ -1,15 +1,24 @@
 % Created by Rajeev Rajendran
 % 2019 July 03
 % split quad data for 3 z-stacks
+% makes the analysis and split folders and saves the splits files in
+% appropriate folders
+
 function quad_split3
-[fn , pn] = uigetfile('*_quadrature.mat'); % select the correct quadrature file
+
+% select the correct quadrature file from the correct raw image folder
+% containing the original sbx and quadrature files
+[fn , pn] = uigetfile('*_quadrature.mat');
+% change current directory to the folder
 cd(pn)
-quad = load(uigetfile('*_quadrature.mat')); % select the correct quadrature file again
+% load the correct quadrature file
+quad = load(uigetfile('*_quadrature.mat')); 
 quad = quad.quad_data;
+% make blank files for the splits
 quad_split1=NaN(1,floor((size(quad,2))/3));
 quad_split2=NaN(1,floor((size(quad,2))/3));
 quad_split3=NaN(1,floor((size(quad,2))/3));
-
+% transfer the correct run file entry to the correct split
 for i=1:(floor(size(quad,2)))
     if floor((i-1)/3)==(i-1)/3
     quad_split1(1,floor(i/3)+1)= quad(1,i);
@@ -19,6 +28,7 @@ for i=1:(floor(size(quad,2)))
     quad_split3(1,floor(i/3)+1)= quad(1,i);
     end 
 end
+% check and/or make split directories
 if exist([pn 'analysis\split1\'])  
 else
     mkdir([pn 'analysis\split1\'])
@@ -31,6 +41,7 @@ if exist([pn 'analysis\split3\'])
 else
     mkdir([pn 'analysis\split3\'])
 end
+% save split files to the correct split directories
 save([pn 'analysis\split1\' fn(1:end-15) '_quad_split1.mat'],'quad_split1')
 save([pn 'analysis\split2\' fn(1:end-15) '_quad_split2.mat'],'quad_split2')
 save([pn 'analysis\split3\' fn(1:end-15) '_quad_split3.mat'],'quad_split3')
